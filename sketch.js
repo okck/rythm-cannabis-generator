54b019567c5116b2d9952b5de26242db7f39b711
Chris Kim <98981974+okck@users.noreply.github.com>
2023-01-07 22:22:35 -0500

    edit-upload-02

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-// Polar Perlin Noise Loop
-// Daniel Shiffman
+// Starter Code:
+// Daniel Shiffman - Polar Perlin Noise Loop
 // https://thecodingtrain.com/CodingChallenges/136-polar-perlin-noise-loops.html
 // https://youtu.be/ZI1dmHv3MeM
 // https://editor.p5js.org/codingtrain/sketches/sy1p1vnQn
@@ -33,28 +33,27 @@ let outerSliderLabel;
 let THCSlider;
 let TCHSliderLabel;
 
+let footer;
+
 // colors
 let indica = 'linear-gradient(rgb(194, 194, 194), 60%, rgb(22, 170, 255))';
 let hybrid = 'linear-gradient(rgb(194, 194, 194), 60%, rgb(162, 22, 255))';
 let sativa = 'linear-gradient(rgb(194, 194, 194), 60%, rgb(255, 14, 227))';
 
 let noiseMax = 2;
-// noiseMax = .5;
-
 let phase = 0;
 let zoff = 0;
 
 function setup() {
   let cnv = createCanvas(1000, 1000).style("-webkit-filter", `url("#svgfilter")`).style("filter", `url("#svgfilter")`);
   cnv.parent(canvasContainer);
-  // cnv.position(-400, -400);
-  // cnv.position(-400, -200);
   cnv.position(-500, -200);
   cnv.position(-500, -300);
 
   // ///// TEXT INPUT ////// //
   textInput = createInput();
   textInput.attribute('placeholder', 'Purple Haze');
+  textInput.attribute('value', 'Purple Haze');
   textInput.parent(form);
 
   textInputLabel = createDiv('Enter strain name');
@@ -80,14 +79,9 @@ function setup() {
   sType.parent(strainTypeX);
 
   //  ////// NOISE MAX INPUT ///// //
-
   noiseMaxSlider = createSlider(0, 10, 5, 0.1);
-  // noiseMaxSlider = createSlider(0, 20, 3, 0.1);
   noiseMaxSlider.input(showNoiseMaxValue);
   noiseMaxSlider.parent(form);
-
-  // noiseMaxLabel = createDiv('noiseMax Slider');
-  // noiseMaxLabel.parent(form);
 
   noiseMaxLabelContainer = createDiv();
   noiseMaxLabelContainer.id('noisemaxLabelContainer');
@@ -98,23 +92,6 @@ function setup() {
   noiseMaxLabelCenter.parent(noiseMaxLabelContainer);
   noiseMaxLabelRight.parent(noiseMaxLabelContainer);
   noiseMaxLabelContainer.parent(form);
-
-  //  ////// INNER INPUT ///// //
-  // innerSlider = createSlider(50, 300, 100);
-  // innerSlider.parent(form);
-  // innerSlider.input(showInnerValue);
-
-  // innerSliderLabel = createDiv('inner Radius Slider');
-  // innerSliderLabel.parent(form);
-  //  ////// OUTER INPUT ///// //
-  // outerSlider = createSlider(200, 400, 200);
-  // outerSlider.attribute('value', '200');
-  // outerSlider.id('outerSlider');
-  // outerSlider.parent(form);
-  // outerSlider.input(showOuterValue);
-
-  // outerSliderLabel = createDiv('outer Radius Slider');
-  // outerSliderLabel.parent(form);
 
   sTHC = createElement('h3', 'THC: 26.5%');
   sTHC.parent(strainTypeX);
@@ -133,10 +110,6 @@ function setup() {
 
 function showNoiseMaxValue(e) {
   let noiseMaxValue = noiseMaxSlider.value();
-  let xmultiplier = map(noiseMaxValue, 0, 10, 0, 2);
-
-  // noiseMaxLabel.html(`noiseMax Slider: ${noiseMaxValue} with multiplier: ${noiseMaxValue * xmultiplier}` );
-  // print(`noiseMax Slider: ${noiseMaxValue} with multiplier: ${noiseMaxValue * xmultiplier}`);
   print(`noiseMax Slider: ${noiseMaxValue}`);
 
   let target = e.target;
@@ -150,9 +123,6 @@ function showNoiseMaxValue(e) {
 }
 
 function showInnerValue(e) {
-  // let innerValue = innerSlider.value();
-  // innerSliderLabel.html(`Inner Radius Slider: ${innerValue}`);
-
   let target = e.target;
   if (e.target.type !== 'range') {
     target = document.getElementById('range')
@@ -164,9 +134,6 @@ function showInnerValue(e) {
 }
 
 function showOuterValue(e) {
-  // let outerValue = outerSlider.value();
-  // outerSliderLabel.html(`Outer Radius Slider: ${outerValue / 10}`);
-
   let target = e.target;
   if (e.target.type !== 'range') {
     target = document.getElementById('range')
@@ -179,10 +146,9 @@ function showOuterValue(e) {
 
 function calcTHC(e) {
   let THCValue = THCSlider.value();
-  // THCSliderLabel.html(`THC Slider: ${map(THCValue, 0, 130, 24.0, 35.0).toFixed(1)}`);
-  // THCSliderLabel.html(`THC Slider: ${map(THCValue, 0, 130, 20.0, 35.0).toFixed(1)}`);
-
   print(`THC Slider: ${map(THCValue, 0, 130, 20.0, 35.0).toFixed(1)}`);
+
+  THCSliderLabel.html(`THC Slider: ${map(THCValue, 0, 130, 20.0, 35.0).toFixed(1)}%`);
 
   let target = e.target;
   if (e.target.type !== 'range') {
@@ -195,7 +161,6 @@ function calcTHC(e) {
 }
 
 function draw() {
-
   translate(width / 2, height / 2);
   stroke(255, 255, 255, 10);
   stroke(255, 200, 200, 10);
@@ -204,7 +169,6 @@ function draw() {
   beginShape();
 
   noiseMax = noiseMaxSlider.value();
-
   multiplier = map(noiseMax, 0, 10, 0, 2);
 
   innerRadius = (200 - THCSlider.value()) * 1.5;
@@ -213,17 +177,7 @@ function draw() {
   for (let a = 0; a < TWO_PI; a+=.01) {
     let xoff = map(cos(a), -1, 1, 0, noiseMax * multiplier);
     let yoff = map(sin(a), -1, 1, 0, noiseMax * multiplier);
-
-    // less noise:
-    // let r = map(noise(xoff, yoff, zoff), 0, 1, 100, 200);
     let r = map(noise(xoff, yoff, zoff), 0, 1, innerRadius, outerRadius);
-
-    // for more noise:
-    // let r = map(noise(xoff, yoff, zoff), 0, 1, 50, 250);
-
-    // max size:
-    // let r = map(noise(xoff, yoff, zoff), 0, 1, 300, 400);
-
     let x = r * cos(a);
     let y = r * sin(a);
     vertex(x, y);
@@ -234,7 +188,6 @@ function draw() {
 
   print(frameCount);
   let fc = frameCount;
-  // if (fc%600 === 0) {
   if (fc%1000 === 0) {
     noLoop();
   }
@@ -243,15 +196,16 @@ function draw() {
 function startStop() {
   noLoop();
   let buttonStart = createButton("generate");
-  // let buttonStop = createButton("stop sketch");
   let buttonClear = createButton("clear");
 
   buttonStart.parent(form);
-  // buttonStop.parent(form);
   buttonClear.parent(form);
+
+  footer = createDiv('rythm cannabis');
+  footer.id('footer');
+  footer.parent(form);
   
   buttonStart.mousePressed(startSketch);
-  // buttonStop.mousePressed(stopSketch);
   buttonClear.mousePressed(clear);
   
   function startSketch() {
@@ -282,10 +236,6 @@ function startStop() {
       gradient.style.background = sativa;
     }
   }
-
-  // function stopSketch() {
-  //   noLoop();
-  // }
 
   function clear() {
     location.reload();
